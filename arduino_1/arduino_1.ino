16936d151367e1191579a865856e395f858b0e04
Luke McDonald <101687942+shrimp332@users.noreply.github.com>
2025-05-28 16:51:45 +1000

    update use json

--- arduino_1/arduino_1.ino
+++ arduino_1/arduino_1.ino
@@ -1,25 +1,30 @@
 #include <Wire.h>
+
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
+
+#include <ArduinoJson.h>
 
 Adafruit_BMP280 bmp;
 
 void setup() {
   Serial.begin(9600);
   Wire.begin();
+  delay(2000);
+
 
   if (!bmp.begin(0x76)) {
     Serial.println("BMP280 not found at address 0x76");
-    while (1);
+    delay(1000);
+    NVIC_SystemReset(); // ARM reset board
   }
-  Serial.println("BMP280 found!");
 }
 
 void loop() {
-  Serial.print("Temp: ");
-  Serial.print(bmp.readTemperature());
-  Serial.print(" °C, Pressure: ");
-  Serial.print(bmp.readPressure() / 100.0F);
-  Serial.println(" hPa");
+  JsonDocument doc;
+  doc["temp"] = bmp.readTemperature();
+  doc["pressure"] = bmp.readPressure() / 100.0F;
+  serializeJson(doc, Serial);
+  Serial.println();
   delay(2000);
 }
