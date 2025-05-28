3614c94af050ab3d440a3b68a71e222f7904a9e7
Luke McDonald <101687942+shrimp332@users.noreply.github.com>
2025-05-28 16:51:54 +1000

    add edge-server

---
+++ edge_device_1/main.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import serial
+import json
+import time
+import mariadb
+
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=3)
+
+
+db_config = {
+    'host': '127.0.0.1',
+    'user': 'root',
+    'password': '1234',
+    'database': 'bmpDB'
+}
+
+
+try:
+    conn = mariadb.connect(**db_config)
+    cursor = conn.cursor()
+except Exception as e:
+    print(e)  # Failed to connect to database
+    exit(1)
+
+
+def insert_data(temp, pressure):
+    cursor.execute(
+        "INSERT INTO Records (temp, pressure) VALUES (?,?)",
+        (temp, pressure))
+    conn.commit()
+
+
+def read_serial():
+    try:
+        line = ser.readline().decode('utf-8').strip()
+        try:
+            data = json.loads(line)
+            temp = data['temp']
+            pressure = data['pressure']
+            return temp, pressure
+        except Exception as e:
+            print(f"\x1b[31merror={e} data={line}\x1b[0m")
+            time.sleep(1)
+    except Exception as e:
+        print(f"\x1b[31merror={e}\x1b[0m")
+
+
+def main():
+    while True:
+        time.sleep(1)
+        temp, pressure = read_serial()
+        insert_data(temp, pressure)
+        print(temp, pressure)
+
+
+if __name__ == '__main__':
+    main()
